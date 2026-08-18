478c3f5b831e2d9ee6ec6d188581ea7f17979286
sunxjnk <116279032+sunxjnk@users.noreply.github.com>
2026-08-18 23:01:22 +0700

    Add files via upload

---
+++ PTIT055.cpp
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    long long x;
+    cin>>x;
+    for(int i = 0; i<31;i++){
+        cout<<x<<" x "<<i<<" = "<<x*i<<endl;
+    }
+    return 0;
+}
