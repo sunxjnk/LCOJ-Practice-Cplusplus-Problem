754150db265407bbab1b75ec859d13c2a86f674a
sunxjnk <116279032+sunxjnk@users.noreply.github.com>
2026-08-19 21:31:52 +0700

    Add files via upload

---
+++ HNUE_TONG.cpp
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+#include<iostream>
+using namespace std;
+int main()
+{
+#ifndef ONLINE_JUDGE
+freopen ("input.txt", "r", stdin);
+freopen ("output.txt", "w", stdout);
+#endif
+int a, b;
+cin>>a>>b;
+cout<<a+b;
+return 0;
+}
