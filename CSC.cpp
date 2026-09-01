d610c78df9ff883ea6ee861718f2fe0b6e9caf64
sunxjnk <116279032+sunxjnk@users.noreply.github.com>
2026-09-01 16:16:41 +0700

    Add files via upload

---
+++ CSC.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b,c,sl,sb,stg;
+    cin>>a>>b>>c;
+    sl = a;
+    sb = c;
+    if(sl<b) sl = b;
+    if(sl<c) sl = c;
+    if(sb>b) sb = b;
+    if(sb>a) sb = a;
+    stg = a+b+c-sl-sb;
+    if(abs(sl-stg)==abs(sb-stg)) cout<<sl+abs(sb-stg)<<endl;
+    else if(abs(sl-stg)> abs(sb-stg)) cout<<sl-abs(sb-stg)<<endl;
+    else if(abs(sl-stg) < abs(sb-stg)) cout<<sb+abs(sl-stg)<<endl;
+    return 0;
+}
