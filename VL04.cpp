53a577ec78582e9525d543082df2a1a0025d4c4d
sunxjnk <116279032+sunxjnk@users.noreply.github.com>
2026-09-07 14:19:48 +0700

    Add files via upload

---
+++ VL04.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    double tong = 0;
+    for(int i = 2; i<=n;i++){
+        tong += (1.0/i);
+    }
+    cout<<fixed<<setprecision(4)<<tong;
+}
