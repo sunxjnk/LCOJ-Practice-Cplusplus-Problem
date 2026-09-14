f61d65e0c0625705082c6837a54498c7736a56f0
sunxjnk <116279032+sunxjnk@users.noreply.github.com>
2026-09-14 14:51:35 +0700

    Add files via upload

---
+++ VT02.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        bool swapped = false;
+
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+
+                swapped = true;
+            }
+        }
+        if (!swapped) {
+            break;
+        }
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i = 0; i<n;i++){
+        cin>>a[i];
+    }
+    bubbleSort(a,n);
+    int l = a[n-1];
+    if(a[0]==a[n-1]) cout<<"NOT FOUND";
+    else{
+        for(int i = n-1;i>-1;i--){
+            if(a[i]-l!=0) {
+                cout<<a[i];
+                break;
+            }
+        }
+    }
+    return 0;
+}
