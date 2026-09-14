803ce4798bdb43aed6f0f75b74f2eeba216940f7
sunxjnk <116279032+sunxjnk@users.noreply.github.com>
2026-09-14 14:37:52 +0700

    Add files via upload

--- VL12.cpp
+++ VL12.cpp
@@ -1,32 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool isPrime(int n){
-    if (n == 1) return false;
-    else if ((n==2)||(n==3)||(n==5)) return true;
-    else if((n%2==0)||(n%3==0)) return false;
-    else{
-        for(int i=3;i*i<=n;i+=2){
-            if(n%i==0) return false;
-            else return true;
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        bool swapped = false;
+
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+
+                swapped = true;
+            }
+        }
+        if (!swapped) {
+            break;
         }
     }
-    return true;
 }
-
 int main(){
     int n;
     cin>>n;
-    n = abs(n);
-    if(n==0) cout<<"INF"<<endl;
-    else if(n==1) cout<<1<<endl;
-    else if(isPrime(n)) cout<<n<<" "<<1<<endl;
-    else{
-        cout<<n<<" ";
-        for(int i = (n/2)+1;i>1;i--){
-            if(n%i==0) cout<<i<<" ";
-        }
-        cout<<1<<endl;
+    int a[n];
+    for(int i = 0; i<n;i++){
+        cin>>a[i];
     }
+    bubbleSort(a,n);
+    cout<<abs(a[0]-a[n-1])<<endl;
     return 0;
 }
